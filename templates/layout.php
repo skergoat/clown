97a627e88d436f49d252f56a4e9c5f2c69be8a76
skergoat <skergoatweb@gmail.com>
2020-10-07 17:58:46 +0700

    fin jour 2

--- templates/layout.php
+++ templates/layout.php
@@ -16,7 +16,7 @@
                <!-- reseaux -->
                <div class="nav__secondary">
                    <div class="d-flex justify-content-between nav__wrapper">
-                       <div class="d-flex">
+                       <div class="d-flex align-items-center pt-1 pb-1">
                             <p class="d-none d-md-block m-0">Suivez-nous sur les reseaux sociaux !</p>
                             <ul class="d-flex m-0 pl-3 list">
                                 <li class="mr-2"><a href="#" class="link-black"><i class="icon-facebook"></i></a></li>
@@ -24,7 +24,7 @@
                                 <li class="mr-2"><a href="#" class="link-black"><i class="icon-instagram"></i></a></li>
                             </ul>
                         </div>
-                        <div class="pr-1"><a class="font-weight-bold link-black bold" href="#"> Se connecter</a> ou <a class="link-black font-weight-bold bold" href="#">S'inscrire</a></div>
+                        <div class="connection"><a class="font-weight-bold link-black bold" href="#"> Se connecter</a> ou <a class="link-black font-weight-bold bold" href="#">S'inscrire</a></div>
                    </div>
                </div>
                <!-- menu -->
@@ -103,65 +103,80 @@
        <?= $content ?>
        
        <!-- footer -->
-       <footer class="footer">
+       <footer class="footer transition">
            <!-- logo footer -->
-           <div class="mx-auto footer-top pt-1 pb-1">
+           <div class="mx-auto footer-top pt-2 pb-4 transition ">
                 <img src="../assets/img/logo-footer.png" class="footer-top__logo pt-2 pb-2">
            </div>
-           <div class="mx-auto footer-top footer-center-first pt-1 pb-3 d-flex">
-                <ul class="p-0">
-                    <li class="font-weight-bold">Plan du site</li>
-                    <li>Accueil</li>
-                    <li>Boutique</li>
-                    <li>Location</li>
-                    <li>Infos Pratiques</li>
-                    <li>Contact</li>
+            <div class="mx-auto footer-top footer-center-first flex-wrap d-flex pb-5 transition">     
+                <ul class="p-0 m-0 transition">
+                    <li class="font-weight-bold pb-2">Plan du site</li>
+                    <li><a href="" class="link-black">Accueil</a></li>
+                    <li><a href="" class="link-black">Boutique</a></li>
+                    <li><a href="" class="link-black">Location</a></li>
+                    <li><a href="" class="link-black">Infos Pratiques</a></li>
+                    <li><a href="" class="link-black">Contact</a></li>
                 </ul> 
-                <ul class="p-0">
-                    <li class="font-weight-bold">Categories</li>
-                    <li>Accessoires & déguisements</li>
-                    <li>Barbes & moustaches</li>
-                    <li>Chapeaux & coiffes</li>
-                    <li>Cotillons</li>
-                    <li>Décorations</li>
-                    <li>Déguisements</li>
-                    <li>Farces et attrapes</li>
-                    <li>Feux d’artifices</li>
-                    <li>Jouets & gadgets</li>
-                    <li>Loups (demi-masques)</li>
-                    <li>Maquillage</li>
-                    <li>Masques</li>
-                    <li>Perruques</li>
+                <ul class="p-0 m-0">
+                    <li class="font-weight-bold pb-2">Categories</li>
+                    <li><a href="" class="link-black">Accessoires & déguisements</a></li>
+                    <li><a href="" class="link-black">Barbes & moustaches</a></li>
+                    <li><a href="" class="link-black">Chapeaux & coiffes</a></li>
+                    <li><a href="" class="link-black">Cotillons</a></li>
+                    <li><a href="" class="link-black">Décorations</a></li>
+                    <li><a href="" class="link-black">Déguisements</a></li>
+                    <li><a href="" class="link-black">Farces et attrapes</a></li>
+                    <li><a href="" class="link-black">Feux d’artifices</a></li>
+                    <li><a href="" class="link-black">Jouets & gadgets</a></li>
+                    <li><a href="" class="link-black">Loups (demi-masques)</a></li>
+                    <li><a href="" class="link-black">Maquillage</a></li>
+                    <li><a href="" class="link-black">Masques</a></li>
+                    <li><a href="" class="link-black">Perruques</a></li>
                 </ul> 
-                <ul class="p-0">
-                    <li class="font-weight-bold">Evenements</li>
-                    <li>Coupe du monde</li>
-                    <li>Halloween</li>
-                    <li>Jour de l’an</li>
-                    <li>Mardi Gras</li>
-                    <li>Nöel</li>
-                    <li>Pourim</li>
-                    <li>St Valentin</li>
+                <ul class="p-0 m-0 mt-sm-4 mt-md-4">
+                    <li class="font-weight-bold pb-2">Evenements</li>
+                    <li><a href="" class="link-black">Coupe du monde</a></li>
+                    <li><a href="" class="link-black">Halloween</a></li>
+                    <li><a href="" class="link-black">Jour de l’an</a></li>
+                    <li><a href="" class="link-black">Mardi Gras</a></li>
+                    <li><a href="" class="link-black">Nöel</a></li>
+                    <li><a href="" class="link-black">Pourim</a></li>
+                    <li><a href="" class="link-black">St Valentin</a></li>
                 </ul> 
-                <ul class="p-0">
-                    <li class="font-weight-bold">Thèmes</li>
-                    <li>Super-héros</li>
-                    <li>Cow-boys & indiens</li>
-                    <li>Disco</li>
-                    <li>Hippie</li>
-                    <li>Années 80</li>
-                    <li>Star Wars</li>
-                    <li>Séries</li>
+                <ul class="p-0 m-0 mt-sm-4 mt-md-4">
+                    <li class="font-weight-bold pb-2">Thèmes</li>
+                    <li><a href="" class="link-black">Super-héros</a></li>
+                    <li><a href="" class="link-black">Cow-boys & indiens</a></li>
+                    <li><a href="" class="link-black">Disco</a></li>
+                    <li><a href="" class="link-black">Hippie</a></li>
+                    <li><a href="" class="link-black">Années 80</a></li>
+                    <li><a href="" class="link-black">Star Wars</a></li>
+                    <li><a href="" class="link-black">Séries</a></li>
                 </ul>
            </div>
-           <div class="footer-center-second mt-3">
-                <div class="container" style="border:2px solid green;">
-                    <div class="help"></div>
-                    <div class="logo-footer-1"></div>
-                    <div class="logo-footer-2"></div>            
+           <div class="footer-center-second">
+                <div class="container transition">
+                    <div class="p-3">
+                        <p class="help__p1 m-2 black orange">Besoin d'aide ?</p>
+                        <p class="help__p2 m-2 light-grey">Lundi - Samedi : 9h30 - 19h</p>
+                        <p class="help__p3 m-2 black">01 47 70 05 93</p>
+                    </div>
+                    <div class="partner-1 m-1">
+                        <img src="../assets/img/trustpilot.png" class="d-md-block d-lg-block d-none mx-auto pt-4 pb-2">
+                        <img src="../assets/img/trustpilot-responsive.png" class="d-md-none d-lg-none mx-auto pt-5 pb-4">
+                    </div>
+                    <div class="partner-2 pt-2 pb-4 d-flex flex-wrap justify-content-lg-between justify-content-xs-start">
+                        <img src="../assets/img/logo-lydia.png">
+                        <img src="../assets/img/logo-paypal.png">
+                        <img src="../assets/img/logo-visa.png">
+                        <img src="../assets/img/logo-mastercard.png">
+                        <img src="../assets/img/logo-americanexpress.png">
+                        <img src="../assets/img/logo-colissimo.png" class="">
+                        <img src="../assets/img/logo-chronopost.png" class="">
+                    </div>            
                 </div>
            </div>
-           <div class="footer-bottom p-3 mt-3" style="text-align:center;font-family:'Nunito Sans Light';font-size:13px;box-shadow: 2px -1px 5px 2px rgba(234,237,244,1);">
+           <div class="footer-bottom p-3 mt-3">
                 Copyright © Clown Montmartre 2020. Tous droits réservés.
            </div>
        </footer>

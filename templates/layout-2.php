a95d84c3289681912c936b73c28bfb51648ce0bc
skergoat <skergoatweb@gmail.com>
2020-10-08 10:36:44 +0700

    modif image

--- templates/layout-2.php
+++ templates/layout-2.php
@@ -3,9 +3,9 @@
 <!-- main section -->
 <section class="main-content" id="main-content-id">
     <div id="spacer-between">
-        <div style="margin: 0 auto;position: relative; width:100%;height:100%;border:2px solid black;">
+        <!-- <div style="margin: 0 auto;position: relative; width:100%;height:100%;border:2px solid black;">
             <img src="../assets/img/background.jpg" style="width:100%;display:block;object-fit:cover;" />
-        </div>
+        </div> -->
     </div>
     <div class="previous pl-sm-3 pl-md-5 pt-4 ml-2 pb-3 mt-2 mb-2" id="main-title-1"><a class='black link-black' href='#'><span class="mr-1 black chevron"><</span> <?= $previous ?></a></div>
     <div class="title pb-4 mt-4" id="main-title-2"><?= $mainTitle ?></div>

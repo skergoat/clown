1f216fb9de385562ec85381d982fb93bbe82b07c
skergoat <skergoatweb@gmail.com>
2020-10-08 10:36:02 +0700

    first solution

--- templates/layout-2.php
+++ templates/layout-2.php
@@ -1,18 +1,15 @@
 <?php ob_start(); ?>
-<!-- <img src="../assets/img/background.jpg" style="width:100%;min-height:90%;display:block;object-fit:cover;position:fixed;" /> -->
-<div id="spacer-between" style="border:2px solid black;overflow:relative;overflow:hidden;position:fixed;max-height:500px;">
-            <img src="../assets/img/background.jpg" style="width:100%;min-height:90%;object-fit:cover;" />
-            <!-- <div style="width: 220px;position: absolute;top: 0;left: -240px;"></div>-->
-        </div>
+
 <!-- main section -->
-<section class="main-content" id="main-content-id" style="background:transparent;!important;">
-    <!--<div id="spacer-between">-->
-        
-    <div style="width:100%;min-height:100%;border:2px solid red;overflow:hidden;position:absolute;top:490px;"></div>
-    <!-- </div> -->
-    <div class="previous pl-sm-3 pl-md-5 pt-4 ml-2 pb-3 mt-2 mb-2" id="main-title-1" style="background:transparent;!important;"><a class='black link-black' href='#'><span class="mr-1 black chevron"><</span> <?= $previous ?></a></div>
-    <div class="title pb-4 mt-4" id="main-title-2" style="background:transparent;!important;"><?= $mainTitle ?></div>
-    <div class="content mt-1 mb-5" id="content-cross" style="background:transparent;!important;">
+<section class="main-content" id="main-content-id">
+    <div id="spacer-between">
+        <div style="margin: 0 auto;position: relative; width:100%;height:100%;border:2px solid black;">
+            <img src="../assets/img/background.jpg" style="width:100%;display:block;object-fit:cover;" />
+        </div>
+    </div>
+    <div class="previous pl-sm-3 pl-md-5 pt-4 ml-2 pb-3 mt-2 mb-2" id="main-title-1"><a class='black link-black' href='#'><span class="mr-1 black chevron"><</span> <?= $previous ?></a></div>
+    <div class="title pb-4 mt-4" id="main-title-2"><?= $mainTitle ?></div>
+    <div class="content mt-1 mb-5" id="content-cross">
         <div class="mx-auto content__product">
             <?= $primaryContent ?>
         </div>

cdd656b6741a28f60e2fea6a61cb9ee87a1d6bb1
skergoat <skergoatweb@gmail.com>
2020-10-08 11:34:47 +0700

    settings almost ok

--- templates/layout-2.php
+++ templates/layout-2.php
@@ -1,10 +1,10 @@
 <?php ob_start(); ?>
-    <div id="spacer-between" style="border:2px solid black;overflow:relative;overflow:hidden;position:fixed;">
-        <img src="../assets/img/background.jpg" style="width:100vw;height:90vh;min-height:550px;" />
+    <div id="spacer-between" style="border: 2px solid black; position: fixed; top: 0px; padding-top: 120px; height: 100%;">
+        <img src="../assets/img/background.jpg" style="width:105%;position:relative;left:-50px;height:90vh;min-height:90vh;" />
     </div>
 <!-- main section -->
 <section class="main-content" id="main-content-id" style="background:transparent;!important;">        
-    <div style="width:100%;min-height:100%;border:2px solid red;overflow:hidden;position:absolute;top:70vh;"></div>
+    <div style="width:100%;min-height:100%;border:2px solid red;overflow:hidden;position:absolute;top: 90vh;"></div>
     <div class="previous pl-sm-3 pl-md-5 pt-4 ml-2 pb-3 mt-2 mb-2" id="main-title-1" style="background:transparent;!important;"><a class='black link-black' href='#'><span class="mr-1 black chevron"><</span> <?= $previous ?></a></div>
     <div class="title pb-4 mt-4" id="main-title-2" style="background:transparent;!important;"><?= $mainTitle ?></div>
     <div class="content mt-1 mb-5" id="content-cross" style="background:transparent;!important;">

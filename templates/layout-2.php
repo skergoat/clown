fec4c024ed59bf81264a565ef7280ba6786fac92
skergoat <skergoatweb@gmail.com>
2020-10-08 14:25:31 +0700

    at last background

--- templates/layout-2.php
+++ templates/layout-2.php
@@ -1,22 +1,24 @@
 <?php ob_start(); ?>
-<div id="parallax-image">
-    <img src="../assets/img/background.jpg" id="parallax-image__img" />
-</div>
-<!-- main section -->
-<section class="main-content" id="main-content-id"> 
-    <!-- white separator -->       
-    <div id="space-between"></div>
-     <!-- link top --> 
-    <div class="previous pl-sm-3 pl-md-5 pt-4 ml-2 pb-3 mt-2 mb-2" id="main-title-1"><a class='black link-black' href='#'><span class="mr-1 black chevron"><</span> <?= $previous ?></a></div>
-    <!-- main title --> 
-    <div class="title pb-4 mt-4" id="main-title-2"><?= $mainTitle ?></div>
-    <!-- primary content --> 
-    <div class="content mt-1 mb-5" id="content-cross">
-        <div class="mx-auto content__product pt-5 pb-5 pl-5 pr-3">
-            <?= $primaryContent ?>
-        </div>
+<div class="background">
+    <div id="parallax-image">
+        <img src="../assets/img/background.jpg" id="parallax-image__img" />        
     </div>
-</section>
+    <!-- main section -->
+    <section class="main-content" id="main-content-id"> 
+        <!-- white separator -->       
+        <div id="space-between"></div>
+        <!-- link top --> 
+        <div class="previous pl-sm-3 pl-md-5 pt-4 ml-2 pb-3 mt-2 mb-2" id="main-title-1"><a class='black link-black' href='#'><span class="mr-1 black chevron"><</span> <?= $previous ?></a></div>
+        <!-- main title --> 
+        <div class="title pb-4 mt-4" id="main-title-2"><?= $mainTitle ?></div>
+        <!-- primary content --> 
+        <div class="content mt-1 mb-5" id="content-cross">
+            <div class="mx-auto content__product pt-5 pb-5 pl-5 pr-3">
+                <?= $primaryContent ?>
+            </div>
+        </div>
+    </section>
+</div>
 <!-- secondary section -->
 <section class="secondary-content">
     <?= $secondaryContent ?>

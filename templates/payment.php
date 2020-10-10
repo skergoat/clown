a582395f7ee4f1b1d99149b05c9f951c75de4bf4
skergoat <skergoatweb@gmail.com>
2020-10-10 10:15:41 +0700

    include radio

--- templates/payment.php
+++ templates/payment.php
@@ -12,9 +12,13 @@
             <!-- title -->
             <h5 class="black pb-2">Choisissez votre mode de paiement !</h5>
             <span class="border-bottom-orange mb-4"></span>
+            <!-- paragraph -->
             <p class="font-14 light">Le montant de votre commande s’élève à <span class="strong-pink bold">28,89€</span>. Vous pouvez la régler en toute sécurité, en utilisant votre carte bancaire ou PayPal.</p>
+            <!-- form -->
             <form class="mt-5">
+                <!-- row 1 -->
                 <div class="row mr-0 ml-0 mb-3">
+                    <!-- credit card -->
                     <div class="col-lg-6 pl-0 pr-3">
                         <div class="pay-choice p-4">
                             <img src="../assets/img/credit-card-icon.png" class="icon-single d-block mx-auto mb-3 mt-2" alt="logo lydia">
@@ -25,7 +29,14 @@
                                 <p class="light font-13 ml-4 mb-1"><a href="#" class="link-black">En savoir plus sur Lydia</a></p>
                             </div>
                         </div>
+                        <!-- radio -->
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="creditcard" id="creditcard" value="creditcard" checked>
+                            <span class="form-check-input checkmark"></span>
+                            <label class="form-check-label font-13" for="creditcard" style="color:#9D9D9D;">M.</label>
+                        </div>
                     </div>
+                    <!-- paypal -->
                     <div class="col-lg-6 pl-3 pr-0">
                         <div class="pay-choice p-4">
                             <img src="../assets/img/paypal-icon.png" class="icon-single d-block mx-auto mb-3 mt-2" alt="logo paypal">
@@ -36,9 +47,17 @@
                                 <p class="light font-13 ml-4 mb-1"><a href="#" class="link-black">En savoir plus sur Paypal</a></p>
                             </div>
                         </div>
+                        <!-- radio -->
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="paypal" id="paypal" value="paypal">
+                            <span class="form-check-input checkmark"></span>
+                            <label class="form-check-label font-13" for="paypal">Mme</label>
+                        </div>
                     </div>
                 </div>
+                <!-- row 2 -->
                 <div class="row mr-0 ml-0 mt-3">
+                    <!-- check -->
                     <div class="col-lg-6 pl-0 pr-3">
                         <div class="pay-choice p-4">
                             <img src="../assets/img/logo-check-icon.png" class="icon-single d-block mx-auto mb-3 mt-2" alt="logo check">
@@ -48,6 +67,12 @@
                                 <img src="../assets/img/logo-check-single.png" class="know-more-icon logo-check-single" alt="logo lydia">
                                 <p class="light font-13 ml-4 mb-1"><a href="#" class="link-black">En savoir plus sur le paiement par chèque</a></p>
                             </div>
+                        </div>
+                        <!-- radio -->
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="check" id="check" value="check">
+                            <span class="form-check-input checkmark"></span>
+                            <label class="form-check-label font-13" for="check">Mme</label>
                         </div>
                     </div>
                 </div>
@@ -66,12 +91,5 @@
 <?php ob_start(); ?>
 <?php $secondaryContent = ob_get_clean(); ?>
 
-<?php ob_start(); ?>
-    <script>
-        $class="select-selected-large";
-        $class2="select-items-large";
-    </script>
-    <script src="../assets/js/custom-select.js"></script>
-<?php $script = ob_get_clean(); ?>
 <!-- include -->
 <?php require('layout/layoutSecond.php'); ?>

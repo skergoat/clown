fe72572fbd76996b43e6e05bd64f81ddaae84974
skergoat <skergoatweb@gmail.com>
2020-10-10 13:15:32 +0700

    good progression in cart

--- templates/cart.php
+++ templates/cart.php
@@ -6,70 +6,64 @@
 <!-- primary content  -->
 <?php ob_start(); ?>
 <div class="content__product container-width mx-auto mt-2">
-    <div class="border-radius-10 m-0 p-xs-4 pb-sm-0 p-5">
-        <table class="table">
+    <div class="border-radius-10 m-0 p-xs-4 pb-sm-0 p-4">
+        <table class="table table-cart-top mt-3">
             <thead>
                 <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                    <th scope="col">Reference</th>
+                    <th scope="col">Article</th>
+                    <th scope="col">Quantite</th>
+                    <th scope="col">Prix TTC</th>
+                    <th scope="col">Prix total TTC</th>
+                    <th scope="col">Supprimer</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                    <td class="light-grey">78</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td class="light-grey">8,50€</td>
+                    <td class="bold">8,50€</td>
+                    <td><i class="icon-trash font-23 pointer" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                    <td class="light-grey">78</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td class="light-grey">3,95€</td>
+                    <td class="bold">3,95€</td>
+                    <td><i class="icon-trash font-23 pointer" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                    <td class="light-grey">78</td>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td class="light-grey">8,50€</td>
+                    <td class="bold">8,50€</td>
+                    <td><i class="icon-trash font-23 pointer" title='supprimer'></i></td>
                 </tr>
             </tbody>
         </table>
-        <div class="row justify-content-end b-red">
-            <div class="b-green">
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
+        <div class="row justify-content-end b-red mt-4 ml-0 mr-0">
+            <div class="b-green col-lg-5">
+                <table class="table table-cart-bottom">
                     <tbody>
                         <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                            <td>Sous-total :</td>
+                            <td>20,95€</td>
                         </tr>
                         <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                            <td>Transport (France métropolitaine) :</td>
+                            <td>7,94€</td>
                         </tr>
                         <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
+                            <td>Total :</td>
+                            <td>28,89€</td>
                         </tr>
                     </tbody>
                 </table>
-                <div class="row">
+                <div class="row mb-5 justify-content-end">
                     <!-- submit -->
                     <div class="form-submit">
                         <button type="submit" class="btn button-orange black mt-4">Envoyer</button>

7c0f959db38369c4d5bdfa21fb27d221bea1f62a
skergoat <skergoatweb@gmail.com>
2020-10-10 08:49:49 +0700

    start pay page

---
+++ templates/payment.php
@@ -0,0 +1,28 @@
+<!-- titles -->
+<?php $title = "home" ?>
+<?php $mainTitle = "<h2 class='text-center black main-title-sm mb-3'>Paiement</h2>" ?>
+<?php $previous = "<a class='black link-black' href='#'><span class='mr-2 black chevron'><</span>Accueil</a>" ?>
+
+<!-- primary content  -->
+<?php ob_start(); ?>
+<div class="content__product container-width mx-auto">
+    <!-- contact form -->
+    <form class="row border-radius-10 m-0 pt-5 pr-xs-3 pr-sm-5 pb-5 pl-3 signin">
+        <?php // include('includes/connexionForm.php'); ?>
+    </form>
+</div>
+<?php $primaryContent = ob_get_clean(); ?>
+
+<!-- secondary content -->
+<?php ob_start(); ?>
+<?php $secondaryContent = ob_get_clean(); ?>
+
+<?php ob_start(); ?>
+    <script>
+        $class="select-selected-large";
+        $class2="select-items-large";
+    </script>
+    <script src="../assets/js/custom-select.js"></script>
+<?php $script = ob_get_clean(); ?>
+<!-- include -->
+<?php require('layout/layoutSecond.php'); ?>

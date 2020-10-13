c9d1fd72c803b0f4438dc0ce135336f9641551a9
skergoat <skergoatweb@gmail.com>
2020-10-13 09:18:17 +0700

    belle prousse slider responsive basic

--- templates/home.php
+++ templates/home.php
@@ -1,16 +1,24 @@
 <?php ob_start(); ?>
-
-<section class="" id="slider"></section>
+<!-- slider -->
+<section class="d-flex align-items-center" id="slider" style="position:relative;">
+    <div class="slider-arrows w-100 d-flex justify-content-between bgd-transparent b-red" style="z-index:3;position:relative;">
+        <div class="chevron chevron-left d-flex align-items-center font-23 bgd-transparent pl-5"><i class="icon-slider icon-left-open pointer pl-3"></i></div>
+        <div class="chevron d-flex align-items-center chevron-right font-23 bgd-transparent pr-5"><i class="icon-slider icon-right-open pointer pr-3"></i></div>
+    </div>
+    <img class="img-fluid" src="../assets/img/placeholder.jpg" alt="" style="min-height:100%;object-fit:cover;z-index:1;position:absolute;" />
+</section>
+<!-- themes logos -->
 <section class="pt-0">
     <div class="container">
         <div class="row p-0 pt-5 pb-5 d-flex flex-row justify-content-between mx-mille mx-auto">
-            <div class="col-3" style="max-width:210px;"><img src="../assets/img/deguisement.png" alt="" class="w-100"></div>
-            <div class="col-3" style="max-width:210px;"><img src="../assets/img/accessoires.png" alt="" class="w-100"></div>
-            <div class="col-3" style="max-width:210px;"><img src="../assets/img/fetes.png" alt="" class="w-100"></div>
-            <div class="col-3" style="max-width:210px;"><img src="../assets/img/promotions.png" alt="" class="w-100"></div>
+            <div class="col-3 p-0 pt-2 pb-3" style="max-width:195px;"><img src="../assets/img/deguisement.png" alt="" class="w-100"></div>
+            <div class="col-3 p-0 pt-2 pb-3" style="max-width:195px;"><img src="../assets/img/accessoires.png" alt="" class="w-100"></div>
+            <div class="col-3 p-0 pt-2 pb-3"style="max-width:195px;"><img src="../assets/img/fetes.png" alt="" class="w-100"></div>
+            <div class="col-3 p-0 pt-2 pb-3" style="max-width:195px;"><img src="../assets/img/promotions.png" alt="" class="w-100"></div>
         </div>
     </div>
 </section>
+<!-- 1st section -->
 <section class="section-3 bgd-yellow pb-5">
     <div class="container bgd-yellow pt-5 pb-2">
         <div class="row content__product container-width mx-auto mt-3 p-5 mb-5 mx-mille">
@@ -26,8 +34,11 @@
                 <img src="../assets/img/placeholder.png" class="img-fluid" alt="Responsive image">
             </div>
         </div>
+        <!-- cards -->
         <div class="bgd-yellow">
+            <!-- title -->
             <h5 class="black pb-2 bgd-yellow m-0 pb-4 mx-mille mx-auto">Découvrez nos produits</h5>
+            <!-- cards 1 -->
             <div class="row bgd-yellow pb-3">
                 <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
                 <div class="col-11 mx-mille mx-auto bgd-yellow">
@@ -44,6 +55,7 @@
                 <div class="bgd-yellow chevron d-flex align-items-center chevron-right font-23"><i class="icon-right-open pointer"></i></div>
             </div>
             <button class="btn btn-cards-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les promos <span class="pl-2 font-10 bold btn-chevron">></span></button>
+            <!-- cards 2 -->
             <div class="row mt-5 mb-4 pb-3 pt-2 bgd-yellow">
                 <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
                 <div class="col-11 mx-mille mx-auto">
@@ -60,6 +72,7 @@
                 <div class="bgd-yellow chevron d-flex align-items-center chevron-right font-23"><i class="icon-right-open pointer"></i></div>
             </div>
             <button class="btn btn-cards-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Tous les déguisements <span class="pl-2 font-10 bold btn-chevron">></span></button>
+            <!-- cards 3 -->
             <div class="row mb-4 mt-5 pb-3 pt-2 bgd-yellow">
                 <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
                 <div class="col-11 mx-mille mx-auto">
@@ -76,6 +89,7 @@
                 <div class="bgd-yellow chevron d-flex align-items-center chevron-right font-23"><i class="icon-right-open pointer"></i></div>
             </div>
             <button class="btn btn-cards-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les articles de fête <span class="pl-2 font-10 bold btn-chevron">></span></button>
+            <!-- cards 4 -->
             <div class="row bgd-yellow pb-3 pt-2 mt-5">
                 <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
                 <div class="col-11 mx-mille mx-auto">

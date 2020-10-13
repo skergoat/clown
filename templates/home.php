f7c7854e3985bb13495a7dd0674497366aca3f48
skergoat <skergoatweb@gmail.com>
2020-10-13 08:49:58 +0700

    go on homepage

--- templates/home.php
+++ templates/home.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
 
-<section class="b-black" id="slider"></section>
+<section class="" id="slider"></section>
 <section class="pt-0">
     <div class="container">
         <div class="row p-0 pt-5 pb-5 d-flex flex-row justify-content-between mx-mille mx-auto">
@@ -12,10 +12,10 @@
     </div>
 </section>
 <section class="section-3 bgd-yellow pb-5">
-    <div class="container bgd-yellow pt-5">
+    <div class="container bgd-yellow pt-5 pb-2">
         <div class="row content__product container-width mx-auto mt-3 p-5 mb-5 mx-mille">
-            <div class="col-md-6 pr-5">
-                <h5 class="black pb-2">Clown Montmartre</h5>
+            <div class="col-md-6 pr-5 pl-4 pb-4">
+                <h3 class="black pb-2 pt-2">Clown Montmartre</h3>
                 <span class="border-bottom-orange mb-4"></span>
                 <p class="line-height pb-3 font-14">
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
@@ -27,8 +27,8 @@
             </div>
         </div>
         <div class="bgd-yellow">
-            <h6 class="black pb-2 bgd-yellow m-0 pb-4 mx-mille mx-auto">Découvrez nos produits</h6>
-            <div class="row bgd-yellow">
+            <h5 class="black pb-2 bgd-yellow m-0 pb-4 mx-mille mx-auto">Découvrez nos produits</h5>
+            <div class="row bgd-yellow pb-3">
                 <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
                 <div class="col-11 mx-mille mx-auto bgd-yellow">
                     <div class="row bgd-yellow justify-content-between">
@@ -43,12 +43,12 @@
                 </div>
                 <div class="bgd-yellow chevron d-flex align-items-center chevron-right font-23"><i class="icon-right-open pointer"></i></div>
             </div>
-            <button class="btn btn-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les promos <span class="pl-2 font-10 bold btn-chevron">></span></button>
-            <div class="row mt-5 mb-4 bgd-yellow">
-            <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
-                    <div class="col-11 mx-mille mx-auto">
+            <button class="btn btn-cards-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les promos <span class="pl-2 font-10 bold btn-chevron">></span></button>
+            <div class="row mt-5 mb-4 pb-3 pt-2 bgd-yellow">
+                <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
+                <div class="col-11 mx-mille mx-auto">
                     <div class="row bgd-yellow justify-content-between">
-                        <div class="p-0 mr-2 bgd-yellow" style="width:24%"><img src="../assets/img/deguisements-card.png" class="img-fluid bs-red br-10" alt="Responsive image"></div>
+                            <div class="p-0 mr-2 bgd-yellow" style="width:24%"><img src="../assets/img/deguisements-card.png" class="img-fluid bs-red br-10" alt="Responsive image"></div>
                             <div class="p-0 mr-1 bs-red br-10 card-product" style="width:24%">
                             </div>
                             <div class="p-0 ml-1 bs-red br-10 card-product" style="width:24%">
@@ -59,39 +59,39 @@
                 </div>
                 <div class="bgd-yellow chevron d-flex align-items-center chevron-right font-23"><i class="icon-right-open pointer"></i></div>
             </div>
-            <button class="btn btn-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Tous les déguisements <span class="pl-2 font-10 bold btn-chevron">></span></button>
-            <div class="row mb-4 mt-5 bgd-yellow">
+            <button class="btn btn-cards-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Tous les déguisements <span class="pl-2 font-10 bold btn-chevron">></span></button>
+            <div class="row mb-4 mt-5 pb-3 pt-2 bgd-yellow">
                 <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
                 <div class="col-11 mx-mille mx-auto">
                     <div class="row bgd-yellow justify-content-between">
                         <div class="p-0 mr-2 bgd-yellow" style="width:24%"><img src="../assets/img/fete-card.png" class="img-fluid bs-red br-10" alt="Responsive image"></div>
-                            <div class="p-0 mr-1 bs-red br-10 card-product" style="width:24%">
-                            </div>
-                            <div class="p-0 ml-1 bs-red br-10 card-product" style="width:24%">
-                            </div>
-                            <div class="p-0 ml-2 bs-red br-10 card-product" style="width:24%">
+                        <div class="p-0 mr-1 bs-red br-10 card-product" style="width:24%">
+                        </div>
+                        <div class="p-0 ml-1 bs-red br-10 card-product" style="width:24%">
+                        </div>
+                        <div class="p-0 ml-2 bs-red br-10 card-product" style="width:24%">
                         </div>
                     </div>
                 </div>
                 <div class="bgd-yellow chevron d-flex align-items-center chevron-right font-23"><i class="icon-right-open pointer"></i></div>
             </div>
-            <button class="btn btn-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les articles de fête <span class="pl-2 font-10 bold btn-chevron">></span></button>
-            <div class="row bgd-yellow mt-5">
+            <button class="btn btn-cards-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les articles de fête <span class="pl-2 font-10 bold btn-chevron">></span></button>
+            <div class="row bgd-yellow pb-3 pt-2 mt-5">
                 <div class="bgd-yellow chevron chevron-left d-flex align-items-center font-23"><i class="icon-left-open pointer"></i></div>
                 <div class="col-11 mx-mille mx-auto">
                     <div class="row bgd-yellow justify-content-between">
                         <div class="p-0 mr-2 bgd-yellow" style="width:24%"><img src="../assets/img/accessoires-card.png" class="img-fluid bs-red br-10" alt="Responsive image"></div>
-                            <div class="p-0 mr-1 bs-red br-10 card-product" style="width:24%">
-                            </div>
-                            <div class="p-0 ml-1 bs-red br-10 card-product" style="width:24%">
-                            </div>
-                            <div class="p-0 ml-2 bs-red br-10 card-product" style="width:24%">
+                        <div class="p-0 mr-1 bs-red br-10 card-product" style="width:24%">
+                        </div>
+                        <div class="p-0 ml-1 bs-red br-10 card-product" style="width:24%">
+                        </div>
+                        <div class="p-0 ml-2 bs-red br-10 card-product" style="width:24%">
                         </div>
                     </div>
                 </div>
                 <div class="bgd-yellow chevron d-flex align-items-center chevron-right font-23"><i class="icon-right-open pointer"></i></div>
             </div>
-            <button class="btn btn-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les accessoires <span class="pl-2 font-10 bold btn-chevron">></span></button>
+            <button class="btn btn-cards-pink bold font-15 d-block mx-auto pl-4 pr-4 mt-4">Toutes les accessoires <span class="pl-2 font-10 bold btn-chevron">></span></button>
         </div>
     </div>
 </section>

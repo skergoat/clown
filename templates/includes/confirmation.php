208aed42c9ff6c9dcfcc3755723183b8c7738f7f
skergoat <skergoatweb@gmail.com>
2020-10-25 18:01:35 +0700

    confirmation page

--- templates/includes/confirmation.php
+++ templates/includes/confirmation.php
@@ -52,7 +52,7 @@
 </div>
 
 <!-- text area -->
-<div class="col-lg-12 mb-xs-4 mb-lg-0 mt-xs-4 mt-sm-4 mt-lg-2 p-xs-none pb-3 pt-2">
+<div class="col-lg-12 mb-xs-4 mb-lg-0 mt-xs-4 mt-sm-4 mt-lg-2 p-xs-none pb-0 pt-2">
     <!-- title -->
     <h5 class="black pb-2">Votre Message</h5>
     <span class="border-bottom-orange mb-1"></span>
@@ -60,166 +60,7 @@
 </div>
 
 <!-- shipping -->
-<div class="col-lg-12 mb-xs-4 mb-lg-0 mt-xs-4 mt-sm-4 mt-lg-3 p-xs-none pb-3 pt-2">
-    <!-- title -->
-    <h5 class="black pb-2">Livraison</h5>
-    <span class="border-bottom-orange mb-1"></span>
-    <!-- shipping methods -->
-    <div class="shipping-methods row mt-4 p-0 m-0">
-        <!-- credit card -->
-        <div class="col-lg-2 mb-xs-3 mb-sm-3 pl-0 pr-xs-0 pr-xs-none pr-sm-0 pr-lg-3">
-            <div id="shipping-colissimo" class="payment-choice shipping-choice pt-2 pb-2 pr-0 pl-0 pointer" data-url="colissimo">
-                <img src="../assets/img/logo-colissimo-small.png" alt="logo colissimo">
-            </div>
-            <!-- radio -->
-            <div class="mt-2 d-flex justify-content-center">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input shipping-selection" type="radio" name="shipping" id="colissimo" value="colissimo">
-                    <span class="form-check-input form-check-input__pay checkmark"></span>
-                </div>
-            </div>
-            <!-- reception options -->
-            <div class="dropdown dropdown-reception mt-3 mx-auto">
-                <button class="btn dropdown-color btn-sm dropdown-toggle regular d-block mx-auto mt-2" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Lieu de réception
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item d-flex justify-content-between" href="#">Domicile</a>
-                    <a class="dropdown-item d-flex justify-content-between" href="#">Commerçant</a>
-                    <a class="dropdown-item d-flex justify-content-between" href="#">La Poste</a>
-                </div>
-            </div>
-        </div>
-        <!-- paypal -->
-        <div class="col-lg-2 mb-xs-3 mb-sm-3 pl-0 pr-xs-0 pr-xs-none pr-sm-0 pr-lg-3">
-            <div id="shipping-chronopost" class="payment-choice shipping-choice pt-2 pb-2 pr-0 pl-0 pointer" data-url="chronopost">
-                <img src="../assets/img/logo-chronopost-small.png" alt="logo chronopost">
-            </div>
-            <!-- radio -->
-            <div class="mt-2 d-flex justify-content-center">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input shipping-selection" type="radio" name="shipping" id="chronopost" value="chronopost">
-                    <span class="form-check-input form-check-input__pay checkmark"></span>
-                </div>
-            </div>
-        </div>
-        <!-- paypal -->
-        <div class="col-lg-2 mb-xs-3 mb-sm-3 pl-0 pr-xs-0 pr-xs-none pr-sm-0 pr-lg-3">
-            <div id="shipping-relais" class="payment-choice shipping-choice pt-2 pb-2 pr-0 pl-0 choice-selected pointer" data-url="relais">
-                <div><img src="../assets/img/point-relais.png" alt="logo relais"></div>
-            </div>
-            <!-- radio -->
-            <div class="mt-2 d-flex justify-content-center">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input shipping-selection" type="radio" name="shipping" id="relais" value="relais" checked="true">
-                    <span class="form-check-input form-check-input__pay checkmark"></span>
-                </div>
-            </div>
-        </div>
-        <!-- paypal -->
-        <div class="col-lg-2 mb-xs-3 mb-sm-3 pl-0 pr-xs-0 pr-xs-none pr-sm-0 pr-lg-3">
-            <div id="shipping-retrait" class="payment-choice shipping-choice pointer d-flex align-items-center justify-content-center" data-url="retrait">
-                <span class="icon-shop" style="color:black!important;font-size:15px;"></span>
-                <p class="black font-12 text-center m-0 ml-2">Retrait en magasin</p>
-            </div>
-            <!-- radio -->
-            <div class="mt-2 d-flex justify-content-center">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input shipping-selection" type="radio" name="shipping" id="retrait" value="retrait">
-                    <span class="form-check-input form-check-input__pay checkmark"></span>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="shipping-find mt-4">
-        <div class="shipping-title row p-2 m-0 d-flex justify-content-center">
-            <h5 class="black font-17 m-0">Sélectionnez votre Point Relais® Mondial Relay.</h5>
-        </div>
-        <div class="shipping-content p-xs-2 p-sm-4">
-            <div class="row p-0 pr-xs-0 pr-lg-4 m-0">
-                <!-- <form> -->
-                <div class="w-100">
-                    <div class="form-row align-items-sm-center flex-xs-column flex-sm-column flex-xl-row pt-xs-3"> 
-                        <div class="d-xs-block d-sm-block d-md-flex col-auto p-0">
-                            <div><img src="../assets/img/big-logo-point-relais.png" alt="logo point relais" class="d-block mx-xs-auto mr-sm-3" id="big-logo-relais"></div>
-                            <div class="d-xs-block d-flex align-items-center mr-xs-0 mr-sm-3 mt-xs-4">
-                                <label for="cityname" class="bold font-13 pr-3 mb-0">Ville</label>
-                                <input type="text" class="search form-control w-100 mb-0 font-12" id="cityname" value="Paris">
-                            </div>
-                            <div class="d-xs-block d-flex align-items-center mt-xs-4">
-                                <label for="zipcode" class="bold font-13 pr-3 mb-0">CP</label>
-                                <input type="text" class="search form-control mb-0 font-12" id="zipcode" value="75001">
-                            </div>
-                        </div>
-                        <div class="d-flex align-items-center align-items-center mt-xs-4">
-                            <div class="col-auto">
-                                <div class="form-check">
-                                    <img src="../assets/img/flag_fr.png" alt="logo point relais" id="flag">
-                                </div>
-                            </div>
-                            <div class="col-auto p-0">
-                                <button type="submit" class="btn button-orange btn-summary black">Chercher</button>
-                            </div>
-                            <div class="col-auto p-0">
-                                <div class="form-check">
-                                    <img src="../assets/img/target.png" alt="logo point relais" id="logo-target">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- </form> -->
-            </div>
-            <div class="search-result row d-sm-flex mt-5 mt-xl-4 p-0 m-0">
-                <div class="d-xs-flex d-sm-flex d-xl-block"> 
-                    <div class="search-text pl-0 col-xl-12 col-lg-12 d-flex flex-column">
-                        <div>
-                            <p class="search-text__title bold mb-2">1- L’univers du mobile</p>
-                            <p class="search-text__content mb-1">1 RUE DES PROUVAIRES</p>
-                            <p class="search-text__content">75001 - PARIS</p>
-                        </div>
-                        <div>
-                            <p class="search-text__title bold mb-2">2- click & services</p>
-                            <p class="search-text__content mb-1">FORUM DES HALLES LESCOT -2</p>
-                            <p class="search-text__content">75001 - PARIS</p>
-                        </div>
-                        <div>
-                            <p class="search-text__title bold mb-2">3- Paris 1 my auchan</p>
-                            <p class="search-text__content mb-1">20 PLACE DU MARCHE SAINT HONORE</p>
-                            <p class="search-text__content">75001 - PARIS</p>
-                        </div>
-                        <div>
-                            <p class="search-text__title bold mb-2">4- Avenir informatique</p>
-                            <p class="search-text__content mb-1">39 RUE BEAUREGARD</p>
-                            <p class="search-text__content">75002 - PARIS</p>
-                        </div>
-                    </div>
-                    <div class="search-text pl-0 col-xl-12 col-lg-12 d-flex flex-column">
-                        <div>
-                            <p class="search-text__title bold mb-2">5- Bodhi telecom</p>
-                            <p class="search-text__content mb-1">2 RUE DE MULHOUSE</p>
-                            <p class="search-text__content">75002 - PARIS</p>
-                        </div>
-                        <div>
-                            <p class="search-text__title bold mb-2">6- My Auchan Reaumur</p>
-                            <p class="search-text__content mb-1">85 RUE REAUMUR</p>
-                            <p class="search-text__content">75002 - PARIS</p>
-                        </div>
-                        <div>
-                            <p class="search-text__title bold mb-2">7- Rufezahoq</p>
-                            <p class="search-text__content mb-1">9 RUE DE BRUXELLES</p>
-                            <p class="search-text__content">75009 - PARIS</p>
-                        </div>
-                    </div>
-                </div>
-                <div id="map"></div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- shipping -->
-<div class="col-lg-12 mb-xs-4 mb-lg-0 mt-xs-4 mt-sm-4 mt-lg-4 p-xs-none pb-0 pt-2">
+<div class="col-lg-12 mb-xs-4 mb-lg-0 mt-xs-4 mt-sm-4 mt-lg-4 p-xs-none pb-0 pt-0">
     <!-- title -->
     <h5 class="black pb-2">Récapitulatif</h5>
     <span class="border-bottom-orange mb-1"></span>
@@ -228,7 +69,7 @@
         <!-- table responsive -->
         <div class="overflow-scroll">
             <!-- table top -->
-            <table class="table table-cart-top mt-3">
+            <table class="table table-cart-top table-cart-top__confirm mt-3">
                 <!-- head -->
                 <thead>
                     <tr>
@@ -239,7 +80,6 @@
                         <th scope="col">Quantité</th>
                         <th scope="col">Prix TTC</th>
                         <th scope="col width-100">Prix total TTC</th>
-                        <th scope="col width-100">Supprimer</th>
                     </tr>
                 </thead>
                 <!-- body -->
@@ -252,7 +92,6 @@
                         <td>1</td>
                         <td class="light-grey">8,50€</td>
                         <td class="bold width-100">8,50€</td>
-                        <td><i class="icon-trash font-23 pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
                         <td class="light-grey">78</td>
@@ -262,7 +101,6 @@
                         <td>1</td>
                         <td class="light-grey">3,95€</td>
                         <td class="bold width-100">3,95€</td>
-                        <td><i class="icon-trash font-23 pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
                         <td class="light-grey">78</td>
@@ -272,7 +110,6 @@
                         <td>1</td>
                         <td class="light-grey">8,50€</td>
                         <td class="bold width-100">8,50€</td>
-                        <td><i class="icon-trash font-23 pointer width-100" title='supprimer'></i></td>
                     </tr>
                 </tbody>
             </table>
@@ -297,19 +134,23 @@
                         </tr>
                     </tbody>
                 </table>
-                <!-- submit -->
-                <div class="row mb-5 mt-4 justify-content-end mr-0 ml-0">
-                    <div class="form-submit mb-xs-4">
-                        <button type="submit" class="btn button-orange btn-orange-outlined black">Mettre à jour</button>
-                    </div>
-                    <div class="form-submit ml-3">
-                        <!-- btn provisoire -->
-                        <a href="http://localhost:8888/templates/payment.php" class="btn button-orange black">Valider</a>
-                        <!-- btn définitif -->
-                        <!-- <button type="submit" class="btn button-orange btn-cart black">Valider</button> -->
-                    </div>
-                </div>
             </div>
         </div>
+    </div>
+</div>
+<!-- shipping -->
+<div class="col-lg-12 mb-xs-4 mb-lg-0 mt-xs-4 mt-sm-4 mt-lg-4 p-xs-none pb-0 pt-0">
+    <!-- title -->
+    <h5 class="black pb-2">Mode de Paiement</h5>
+    <span class="border-bottom-orange mb-1"></span>
+    <div class="d-flex">
+        <!-- submit -->
+        <div class="mt-3 mr-4"><img src="../assets/img/logo-check-single.png" class="know-more-icon logo-check-single mb-2" alt="logo lydia"><span class="ml-2 font-17 bold">Chèque</span></div>
+        <!-- btn provisoire -->
+        <div>
+            <button type="submit" class="btn button-orange btn-cart-outlined btn-cb black">Payer par Carte Bleue</button>
+            <!-- btn définitif -->
+            <!-- <button type="submit" class="btn button-orange btn-cart black">Valider</button> -->
+        </div> 
     </div>
 </div>

f73b4b0a49028d72b30e294824d1d40a042c039e
skergoat <skergoatweb@gmail.com>
2020-10-27 13:59:06 +0700

    orange link on account page

--- templates/includes/myAccount.php
+++ templates/includes/myAccount.php
@@ -1,12 +1,12 @@
 <!-- formulaire -->
-<div class="mx-auto col-lg-10 mb-xs-4 mb-lg-5 mt-xs-4 mt-sm-4 mt-lg-0 p-xs-none pl-xs-none pl-lg-5 pb-3 pt-2">
+<div class="mx-auto col-lg-10 mb-xs-4 mb-lg-5 mt-xs-4 mt-sm-4 mt-lg-0 p-xs-none pl-xs-none pt-0 pr-4 pl-4 pb-0" style="border-radius:20px;box-shadow:0px 4px 12px 5px rgba(167, 28, 28, 0.21) !important;">
     <div class="row">
         <div class="col-6 p-2 d-flex justify-content-center align-items-center">
             <div style="max-width:200px;">
                 <img src="../assets/img/avatar.png" alt="logo colissimo" style="width:100%;border-radius:200px;box-shadow:0px 4px 12px 5px rgba(167, 28, 28, 0.21) !important;">
             </div>
         </div>
-        <div class="col-6">
+        <div class="col-6 pt-4">
             <!-- title -->
             <h5 class="black pb-2">Coordonnées</h5>
             <span class="border-bottom-orange mb-4"></span>
@@ -33,6 +33,11 @@
                         <td><span class="bold">Adresse de Facturation</span></td>
                         <td>20 rue de Paris, 75015 Paris, FRANCE</td>
                     </tr>
+                    <tr>
+                        <td>
+                            <a href="http://localhost:8888/templates/editAccount.php" class="btn button-orange btn-summary black mt-2 mb-2">Modifier</a>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>    
@@ -40,14 +45,14 @@
 </div>
 
 <!-- formulaire -->
-<div class="mx-auto col-lg-12 mb-xs-4 mb-lg-0 mt-xs-4 mt-sm-4 mt-lg-0 p-xs-none pl-xs-none pl-lg-4 pb-3 pt-2">
-    <div class="col-12 mb-4">
+<div class="mx-auto col-lg-12 mb-xs-4 mb-lg-4 mt-xs-4 mt-sm-4 mt-lg-5 p-xs-none pl-xs-none pl-lg-4 pr-lg-4 pb-3 pt-2">
+    <div class="col-12 mb-5">
         <h5 class="black pb-2 text-center">Adresses de livraison</h5>
         <span class="border-bottom-orange mb-4 mx-auto"></span>
     </div>
     <div class="row p-0 m-0">
         <!-- shipping address -->
-        <div class="col-lg-6 pl-lg-0 mb-xs-none mb-sm-0 mb-lg-4 mb-lg-0 pl-xs-none pr-xs-none pr-sm-0 pr-lg-5 pt-2">
+        <div class="col-lg-6 pl-lg-2 mb-xs-none mb-sm-0 mb-lg-4 mb-lg-0 pl-xs-none pr-xs-none pr-sm-0 pr-lg-5 pt-2">
             <!-- title -->
             <h5 class="black pb-2 font-17">Ajouter une adresse de livraison</h5>
             <!-- <span class="border-bottom-orange mb-4"></span> -->
@@ -129,42 +134,42 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td class="pt-3 pb-3"><a href="#" class="bold action-link" title="modifier" id="link-modal" data-toggle="modal" data-target="#edit-address-modal">Maison</a></td>
+                        <td class="pt-3 pb-3"><a href="#" class="black action-link link-orange" title="modifier" id="link-modal" data-toggle="modal" data-target="#edit-address-modal">Maison</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
-                        <td class="pt-3 pb-3"><a href="#" class="bold action-link"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Amazon</a></td>
+                        <td class="pt-3 pb-3"><a href="#" class="black action-link link-orange"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Amazon</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
-                        <td class="pt-3 pb-3"><a href="#" class="bold action-link"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Chez Maman</a></td>
+                        <td class="pt-3 pb-3"><a href="#" class="black action-link link-orange"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Chez Maman</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
-                        <td class="pt-3 pb-3"><a href="#" class="bold action-link"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Deliveroo</a></td>
+                        <td class="pt-3 pb-3"><a href="#" class="black action-link link-orange"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Deliveroo</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
-                        <td class="pt-3 pb-3"><a href="#" class="bold action-link"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Maison</a></td>
+                        <td class="pt-3 pb-3"><a href="#" class="black action-link link-orange"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Maison</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr> 
-                        <td class="pt-3 pb-3"><a href="#" class="bold action-link"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Amazon</a></td>
+                        <td class="pt-3 pb-3"><a href="#" class="black action-link link-orange"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Amazon</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
-                        <td class="pt-3 pb-3"><a href="#" class="bold action-link"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Chez Maman</a></td>
+                        <td class="pt-3 pb-3"><a href="#" class="black action-link link-orange"  title="modifier" data-toggle="modal" data-target="#edit-address-modal">Chez Maman</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
                     <tr>
-                        <td class="pt-3 pb-3"><a href="#"  class="bold action-link" title="modifier" data-toggle="modal" data-target="#edit-address-modal">Deliveroo</a></td>
+                        <td class="pt-3 pb-3"><a href="#"  class="black action-link link-orange" title="modifier" data-toggle="modal" data-target="#edit-address-modal">Deliveroo</a></td>
                         <td class="pt-3 pb-3">20 rue de Paris...</td>
                         <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                     </tr>
@@ -173,7 +178,7 @@
         </div>   
     </div>
 </div>
-
+<hr>
 <!-- formulaire -->
 <div class="mx-auto col-lg-12 mb-xs-4 mb-lg-5 mt-xs-4 mt-sm-4 mt-lg-0 p-xs-none pl-xs-none pl-lg-5 pb-3">
     <div class="col-12 mb-5">
@@ -191,42 +196,42 @@
             </thead>
             <tbody>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905071</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905071</a></td>
                     <td class="pt-3 pb-3">23/11/2020</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905070</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905070</a></td>
                     <td class="pt-3 pb-3">02/10/2020</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905069</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905069</a></td>
                     <td class="pt-3 pb-3">14/08/2020</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905068</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905068</a></td>
                     <td class="pt-3 pb-3">25/05/2020</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905067</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905067</a></td>
                     <td class="pt-3 pb-3">01/04/2020</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905066</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905066</a></td>
                     <td class="pt-3 pb-3">10/03/2020</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905065</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905065</a></td>
                     <td class="pt-3 pb-3">03/01/2020</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>
                 <tr>
-                    <td class="pt-3 pb-3"><a href="#" class="bold" title="modifier">905064</a></td>
+                    <td class="pt-3 pb-3"><a href="#" class="black link-orange" title="modifier">905064</a></td>
                     <td class="pt-3 pb-3">20/11/2019</td>
                     <td class="pt-3 pb-3"><i class="icon-trash font-17 strong-pink pointer width-100" title='supprimer'></i></td>
                 </tr>

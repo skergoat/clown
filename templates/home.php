4a4a039d4afeec7cb826266a015ac60b8f879cc0
skergoat <skergoatweb@gmail.com>
2020-10-28 14:38:28 +0700

    comment

--- templates/home.php
+++ templates/home.php
@@ -17,26 +17,6 @@
         </li>
     </ul>
 </div>
-<!-- slider -->
-<!-- <section class="d-flex align-items-center" id="slider" style="position:relative;">
-    <div class="slider-arrows w-100 d-flex justify-content-between bgd-transparent">
-        <div class="chevron d-flex align-items-center font-23 bgd-transparent pl-xs-2 pl-5"><i class="icon-slider icon-left-open pointer"></i></div>
-        <div class="chevron d-flex align-items-center font-23 bgd-transparent pr-xs-2 pr-5"><i class="icon-slider icon-right-open pointer"></i></div>
-        <div class="slider-control bgd-transparent">
-            <h5 class="black pb-3 slider-control-title">Une rentrée à prix cassé ! Découvrez nos meilleures promos !</h5>
-            <button class="btn btn-pink bold font-15 pl-4 pr-4 d-block mx-auto">Découvrir</button>
-            <div class="slider-control-navs bgd-transparent d-flex justify-content-center align-items-center">
-                <div class="slider-navs bgd-transparent d-flex justify-content-between">
-                    <span class="d-block slider-navs__span active"></span>
-                    <span class="d-block slider-navs__span"></span>
-                    <span class="d-block slider-navs__span"></span>
-                    <span class="d-block slider-navs__span"></span>
-                </div>
-            </div>
-        </div>
-    </div>
-    <img class="img-fluid img-placeholder" src="../assets/img/placeholder.jpg" alt="" />
-</section>-->
 <!-- themes logos -->
 <section class="pt-0">
     <div class="container">
